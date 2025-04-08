e765bdf470f7c66e25e762ffb4be6949d05b09c4
Arian Rodriguez <arianmrv12@gmail.com>
2025-04-08 11:23:33 -0500

    chore: first commit

---
+++ app/src/main/java/com/innovatech/peaceapp/HttpUri.kt
@@ -0,0 +1,10 @@
+package com.innovatech.peaceapp
+
+object HttpUri {
+    const val url: String = "http://192.168.1.54:8080/"
+}
+
+// https://peaceapp-production.up.railway.app/swagger-ui/index.html
+// https://peaceapp-production.up.railway.app/
+
+// http://192.168.1.54:8080/
